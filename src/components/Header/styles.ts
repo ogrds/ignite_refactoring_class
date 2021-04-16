04d3d1e1f0750b77765656679692f556219385e7
Gabriel Ribeiro <gabrielrsilva98@gmail.com>
2021-04-15 23:54:57 -0300

    Migração JS -> TS / CC -> FC

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div `
   background: #c72828;
   padding: 30px 0;
 
--- src/components/ModalAddFood/styles.ts
+++ src/components/ModalAddFood/styles.ts
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
-import { Form as Unform } from '@unform/web';
+import styled from "styled-components";
+import { Form as Unform } from "@unform/web";
 
-export const Form = styled(Unform)`
+export const Form = styled(Unform)
+`
   padding: 48px 40px;
   display: flex;
   flex-direction: column;
